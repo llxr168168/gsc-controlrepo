556cde4b5401ebe47d84a9bd0416c1136f46c6de
Matthew Cengic <matt.cengic@sherwin.com>
2018-10-18 15:57:56 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -25,7 +25,7 @@ unless pick_default($::custom_admin_org, hiera('custom_admin_org',undef)){
 unless $::custom_admin_org {
        fail('NOTICE: You have not set custom_admin_org. Please see http://tswiki.sherwin.com/index.php/Puppet5#facts.d')
 }
-if $::custom_admin_org != 'gsc' {
+if $::custom_admin_org != 'gsc' and $::custom_admin_org != 'val' {
        fail('custom_admin_org is set to something other than gsc but you are using the gsc control repo! Too scary, I quit.')
 }
 
