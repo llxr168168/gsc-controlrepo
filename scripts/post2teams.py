4b62896b2e16ab965e380d242a50949c337343d7
Matt Cengic <matt.cengic@sherwin.com>
2021-11-02 10:34:01 -0400

    Update post2teams.py

--- scripts/post2teams.py
+++ scripts/post2teams.py
@@ -4,8 +4,8 @@ import requests
 import sys
 import os
 
-myurl="https://outlook.office.com/webhook/399c3961-0be6-4edf-ab84-bef4f54a43e7@44b79a67-d972-49ba-9167-8eb05f754a1a/IncomingWebhook/abccc6c3b5f94132a2b7acc920dbe6d4/3fd93c83-fda6-4bd8-8312-f15e20e9ae80"
-myurl="https://outlook.office.com/webhook/cf7b8012-14cd-4389-b600-539c49589f4a@44b79a67-d972-49ba-9167-8eb05f754a1a/JenkinsCI/5451ce3e989745ce83be0864f0ba8135/3fd93c83-fda6-4bd8-8312-f15e20e9ae80"
+
+myurl="https://swcompany.webhook.office.com/webhookb2/cf7b8012-14cd-4389-b600-539c49589f4a@44b79a67-d972-49ba-9167-8eb05f754a1a/JenkinsCI/5451ce3e989745ce83be0864f0ba8135/3fd93c83-fda6-4bd8-8312-f15e20e9ae80"
 mytitle=str(sys.argv[1])
 mybody=str(sys.argv[2])
 myurlpost=str(os.environ["JOB_URL"])
